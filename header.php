50c3d9b9240c527c8bcceb5f9784a9c7f79d34e8
OmarElsayed16 <54691173+OmarElsayed16@users.noreply.github.com>
2024-07-30 18:07:03 -0400

    new header

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <nav id="navbar">
     <ul>
         <li><a href="dashboard.php">Home</a></li>
-        <li><a href="">OPT 2</a></li>
-        <li><a href="">OPT 3</a></li>
+        <li><a href="/forum.html">Forums</a></li>
+        <li><a href="/upload.php">Upload</a></li>
         <li><a href="contact.php">Contact Us</a></li>
         <li><a href="logout.php"><button id="logout-button">Log Out</button></a></li>
     </ul>
